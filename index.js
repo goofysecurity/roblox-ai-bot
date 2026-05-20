fe7b63c4d1fadc0cd128c87495119e0d2c15296f
goofysecurity <sigmalaw67@gmail.com>
2026-05-20 19:13:52 +0000

    Add Node.js entrypoint

---
+++ index.js
@@ -0,0 +1,14 @@
+// index.js
+// Simple Node.js entrypoint example
+
+const http = require('http');
+const port = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello from Node.js!');
+});
+
+server.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
