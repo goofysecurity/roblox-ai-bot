13d92245371bbf12d7d2387f937c471b659c7b7f
goofysecurity <sigmalaw67@gmail.com>
2026-05-23 01:57:39 +0800

    Add chatbot server

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ Rules:
 - No anime or femboy personality
 - Speak clearly and naturally
 - Be short, helpful, and professional
-- Maximum 3-4 sentences
+- Maximum 1-2 sentences
             `
           },
           {
